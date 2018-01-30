3a0800399d684d3f42922ba853580fba7dfb4e22
Myungho Lee <myungho@knights.ucf.edu>
2018-01-30 01:51:23 -0500

    pifacedigitalio rpio test

---
+++ test.py
@@ -0,0 +1,27 @@
+import pifacedigitalio
+import RPIO
+
+
+def socket_callback(socket, val):
+    print("socket %s: '%s'" % (socket.fileno(), val))
+    socket.send("echo: %s\n" % val)
+    #RPIO.close_tcp_client(socket.fileno())
+    if val == "TurnOn":
+    	pfd.relays[1].value = 1
+	elif val == "TurnOff":
+		pfd.relays[1].value = 0
+	elif val == "Disconnect":
+		RPIO.close_tcp_client(socket.fileno())
+	else
+		socket.send("Not supported command")
+	
+
+
+# creates a PiFace Digtal object
+pfd = pifacedigitalio.PiFaceDigital()
+
+# One TCP interrupt callback (opens socket server at port 8080)
+RPIO.add_tcp_callback(8080, socket_callback)
+
+# Starts waiting for interrupts (exit with Ctrl+C)
+RPIO.wait_for_interrupts()
